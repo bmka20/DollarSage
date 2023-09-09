4641ab13af371dd6770bc02abf4df0b4fc31c922
craig-a-lee <craigleeja@gmail.com>
2023-09-09 15:56:57 -0400

    profile updates

--- dollarsage-app/app/profile/page.tsx
+++ dollarsage-app/app/profile/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './style.css'
+
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className="profile">
+      <header>
+        <h1>My Profile</h1>
+      </header>
+      <main>
+          <img
+            src="https://via.placeholder.com/150"
+            alt="Profile Picture"
+            className="profile-picture"
+          />
+        <section className="profile-info">
+          <h2>John Doe</h2>
+          <p>Score:</p>
+          <p>Rank:</p>
+        </section>
+      </main>
+    </div>
   )
 }
 
